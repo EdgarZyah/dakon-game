2dab274851c48046ec6ee2a93185ce06c112836d
Edgar <edgarzyah@gmail.com>
2024-07-07 18:34:27 +0700

    modify the marbles using randomized png from marbles.json

--- src/components/Pit.js
+++ src/components/Pit.js
@@ -1,48 +1,43 @@
-
-
-// import React from 'react';
-
-// const Pit = ({ stones, onClick }) => {
-//     return (
-//         <div className="pit" onClick={onClick}>
-//             {stones}
-//         </div>
-//     );
-// };
-
-// export default Pit;
-
-
-
-
 import React from 'react';
+import './Pit.css';
+import marble1 from './image/red-marble.png';
+import marble2 from './image/blue-marble.png';
+import marble3 from './image/green-marble.png';
+import marble4 from './image/purple-marble.png';
+import marble5 from './image/yellow-marble.png';
+
+const images = [marble1, marble2, marble3, marble4, marble5];
 
 const Pit = ({ stones, onClick }) => {
-    const generateRandomColor = () => {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
+    const getRandomImage = () => {
+        return images[Math.floor(Math.random() * images.length)];
+    };
+
+    const getRandomPosition = () => {
+        const maxPosition = 170;  // 200 (pit size) - 30 (marble size) = 170
+        const top = Math.floor(Math.random() * maxPosition);
+        const left = Math.floor(Math.random() * maxPosition);
+        return { top, left };
     };
 
     return (
         <div className="pit" onClick={onClick}>
-            {Array.from({ length: stones }).map((_, index) => (
-                <div
-                    key={index}
-                    className="marble"
-                    style={{
-                        backgroundImage: 'url(./image/purple-marble.png)',
-                        backgroundColor: generateRandomColor()
-                    }}
-                />
-            ))}
+            {Array.from({ length: stones }).map((_, index) => {
+                const { top, left } = getRandomPosition();
+                return (
+                    <div
+                        key={index}
+                        className="marble"
+                        style={{
+                            backgroundImage: `url(${getRandomImage()})`,
+                            top: `${top}px`,
+                            left: `${left}px`
+                        }}
+                    />
+                );
+            })}
         </div>
     );
 };
 
 export default Pit;
-
-
